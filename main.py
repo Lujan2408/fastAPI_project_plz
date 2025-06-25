0c7a40229202d529d0a415b3bfe4a66f66da26b4
Jose David <lujan9495@gmail.com>
2025-06-25 11:32:24 -0500

    Add timezone functionality to main.py with /time/{iso_code} endpoint

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
   return {"message": "Hello"}
+
+country_timezones = {
+  "CO": "America/Bogota",
+  "MX": "America/Mexico_City",
+  "AR": "America/Buenos_Aires",
+  "BR": "America/Sao_Paulo",
+  "CL": "America/Santiago",
+  "PE": "America/Lima",
+  "EC": "America/Guayaquil",
+}
+
+@app.get("/time/{iso_code}")
+async def get_time(iso_code: str):
+  iso = iso_code.upper()
+  timezone_str = country_timezones.get(iso)
+  tz = ZoneInfo(timezone_str)
+  return {"Time": datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")}
