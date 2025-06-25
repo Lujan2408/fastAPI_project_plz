602e4435d153db22ebc0cbcb84ab530ad98a0958
Jose David <lujan9495@gmail.com>
2025-06-25 16:19:57 -0500

    Refactor customer model to inherit from SQLModel for database integration

--- models/customer.py
+++ models/customer.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, EmailStr
+from sqlmodel import SQLModel
 
-class CustomerBase(BaseModel):
+class CustomerBase(SQLModel):
   name: str
   description: str | None
   email: EmailStr
   age: int
 
-class Customer(CustomerBase):
+class Customer(CustomerBase, table=True):
   id: int | None = None
 
 class Customer_create(CustomerBase):
