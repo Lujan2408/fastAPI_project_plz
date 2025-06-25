d5de902f756f730f0c97ff64cb137ec8188956a2
Jose David <lujan9495@gmail.com>
2025-06-25 16:48:28 -0500

    Refactor /customers endpoint to use SQLModel for database queries; update create_customer to commit new customers to the database

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from models.customer import Customer_create, Customer
 from models.transaction import Transaction, Invoice
 from db import SessionDependency, create_db_and_tables
+from sqlmodel import select
 
 app = FastAPI(lifespan=create_db_and_tables)
 
@@ -11,9 +12,9 @@ db_customers: list[Customer] = []
 # Get methods ⬇️
 
 @app.get("/customers", response_model=list[Customer])
-async def list_customers():
-  return db_customers
-
+async def list_customers(session: SessionDependency):
+  return session.exec(select(Customer)).all()
+  
 @app.get("/customers/{customer_id}", response_model=Customer)
 async def get_customer_by_id(id: int):
   customer = next((customer for customer in db_customers if customer.id == id), None)
@@ -25,9 +26,9 @@ async def get_customer_by_id(id: int):
 async def create_customer(customer_data: Customer_create, session: SessionDependency): 
   # Validate the customer data using the Customer model, inside we pass a dictionary
   customer = Customer.model_validate(customer_data.model_dump())
-  #  Assumming that this is a database, we need to increment the id
-  customer.id = len(db_customers) 
-  db_customers.append(customer) # append allows us to add the customer to the database
+  session.add(customer)
+  session.commit()
+  session.refresh(customer)
   return customer
 
 @app.post("/transactions")
